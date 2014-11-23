73e66869f672ea010bf28806072bd0476935ea28
Robin Burchell <robin+git@viroteck.net>
2014-11-22 19:41:07 -0800

    Split QTuioCursor to a seperate header.

--- main.cpp
+++ main.cpp
@@ -36,69 +36,12 @@
 #include <QUdpSocket>
 #include <QLoggingCategory>
 
+#include "qtuiocursor_p.h"
 #include "qoscbundle_p.h"
 
 Q_LOGGING_CATEGORY(lcTuioSource, "qt.qpa.tuio.source")
 Q_LOGGING_CATEGORY(lcTuioAlive, "qt.qpa.tuio.alive")
 Q_LOGGING_CATEGORY(lcTuioSet, "qt.qpa.tuio.set")
-
-class QTuioCursor
-{
-public:
-    QTuioCursor(int id = -1)
-        : m_id(id)
-        , m_x(0)
-        , m_y(0)
-        , m_vx(0)
-        , m_vy(0)
-        , m_acceleration(0)
-        , m_state(Qt::TouchPointPressed)
-    {
-    }
-
-    int id() const { return m_id; }
-
-    void setX(float x)
-    {
-        if (state() == Qt::TouchPointStationary &&
-            !qFuzzyCompare(m_x + 2.0, x + 2.0)) { // +2 because 1 is a valid value, and qFuzzyCompare can't cope with 0.0
-            setState(Qt::TouchPointMoved);
-        }
-        m_x = x;
-    }
-    float x() const { return m_x; }
-
-    void setY(float y)
-    {
-        if (state() == Qt::TouchPointStationary &&
-            !qFuzzyCompare(m_y + 2.0, y + 2.0)) { // +2 because 1 is a valid value, and qFuzzyCompare can't cope with 0.0
-            setState(Qt::TouchPointMoved);
-        }
-        m_y = y;
-    }
-    float y() const { return m_y; }
-
-    void setVX(float vx) { m_vx = vx; }
-    float vx() const { return m_vx; }
-
-    void setVY(float vy) { m_vy = vy; }
-    float vy() const { return m_vy; }
-
-    void setAcceleration(float acceleration) { m_acceleration = acceleration; }
-    float acceleration() const { return m_acceleration; }
-
-    void setState(const Qt::TouchPointState &state) { m_state = state; }
-    Qt::TouchPointState state() const { return m_state; }
-
-private:
-    int m_id;
-    float m_x;
-    float m_y;
-    float m_vx;
-    float m_vy;
-    float m_acceleration;
-    Qt::TouchPointState m_state;
-};
 
 class TuioSocket : public QObject
 {
